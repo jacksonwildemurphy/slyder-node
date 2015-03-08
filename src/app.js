7112e08b598b4746e7794b275f0477a51b93ec3b
Jackson Murphy <jacksonmurphy1@gmail.com>
2015-03-08 16:51:19 -0600

    Added handlebars template engine and 404,500 error handlers

--- src/app.js
+++ src/app.js
@@ -1,1 +1,49 @@
-// This file is the entry point for the Liyen website
+// **This file is the entry point for the Liyen website**
+
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+
+// Set up the handlebars view engine, set the layout to main.handlebars, and
+// add a helper for section views.
+var handlebars = require('express3-handlebars')
+    .create({
+        defaultLayout: 'main',
+        helpers: {
+            section: function(name, options) {
+                if (!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
+    });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+// Add the 'static' middleware
+app.use(express.static(__dirname + '/public'));
+
+
+
+/// ERROR HANDLERS
+
+// 404 catch-all handler
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+// 500 error handler
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+/// START THE APP
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port'));
+});
